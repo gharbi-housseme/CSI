6e642db327812b45ad00ce843c3c9e068575d66c
OlfaBensoussia <gharbi.housseme@ensi-uma.tn>
2020-03-27 17:18:12 +0100

    v-1

--- csi-master/src/environments/environment.prod.ts
+++ csi-master/src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyBKvvZmFG3tAIJ7duTFdIadf10wZJSWPnQ",
-    authDomain: "projcsi-29c89.firebaseapp.com",
-    databaseURL: "https://projcsi-29c89.firebaseio.com",
-    projectId: "projcsi-29c89",
-    storageBucket: "projcsi-29c89.appspot.com",
-    messagingSenderId: "66062639937",
-    appId: "1:66062639937:web:55206dcb162fa6b007b7c0",
-    measurementId: "G-NV972TZRJZ"
-  }
 };
