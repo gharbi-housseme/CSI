424add8b4bfb9e54127f11533ecf2c9445a2c6e6
zaineb <zaineb.mahdouani@ensi-uma.tn>
2020-03-27 21:16:03 +0100

    updated buttons

--- csi-master/src/app/home/home.component.ts
+++ csi-master/src/app/home/home.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   toggler : boolean = false
   formations = [
-    {img: "https://www.hubone.fr/data/2017/07/algorithme_cryptographie-1-3.jpg",title:"Cryptographie", delay: "animation-delay:0s"},
-    {img: "https://media.threatpost.com/wp-content/uploads/sites/103/2018/10/16160437/code-security.jpeg",title:"Steganographie",delay: "animation-delay:0.3s"},
+    {img: "https://www.hubone.fr/data/2017/07/algorithme_cryptographie-1-3.jpg",title:"Cryptography", delay: "animation-delay:0s"},
+    {img: "https://media.threatpost.com/wp-content/uploads/sites/103/2018/10/16160437/code-security.jpeg",title:"Steganography",delay: "animation-delay:0.3s"},
     {img: "http://www.aslitsecurity.com/images/web-penetration-testing.jpg",title:"Web & Networking",delay: "animation-delay:0.6s"}
     
   ]
