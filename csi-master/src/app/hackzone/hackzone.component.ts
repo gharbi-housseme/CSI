51c0477e8b77a4d2289af699fd9111a154d79a7f
zaineb <zaineb.mahdouani@ensi-uma.tn>
2020-03-28 04:27:04 +0100

    updated

--- csi-master/src/app/hackzone/hackzone.component.ts
+++ csi-master/src/app/hackzone/hackzone.component.ts
@@ -18,25 +18,34 @@ export class HackzoneComponent implements OnInit {
     this.startCountdown(this.period);
   }
   startCountdown(seconds: number){
-    var counter = seconds;
-    var t;
-    var interval = setInterval(() => {
-      counter--;
-      if(counter > 0 ){  
-        t=counter;
-        this.second=(t)%60;
-        t=(t-(t)%60)/60;
-        this.minute=(t)%60;
-        t=(t-(t)%60)/60;
-        this.hour=(t)%24;
-        t=(t-(t)%24)/24;
-        this.day=t;
-      }
+    
+  var countDownDate = new Date("Apr 11, 2020 14:00:00").getTime();
+    
+  // Update the count down every 1 second
+  var x = setInterval(function() {
   
-      if(counter <= 0 ){     
-        clearInterval(interval);
-        console.log('Ding!');
-      };
-    }, 1000);
-  };
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+    document.getElementById("days").innerHTML = days + " "; 
+    document.getElementById("hours").innerHTML = hours + " ";
+    document.getElementById("minutes").innerHTML = minutes + " ";
+    document.getElementById("seconds").innerHTML = seconds + " ";
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000); };
+  
 }
--- csi-master/src/environments/environment.prod.ts
+++ csi-master/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   
-=======
->>>>>>> 6e642db327812b45ad00ce843c3c9e068575d66c
 };
